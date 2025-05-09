98bdfa72c7c8a72a717bb9f8b7888648d694f692
HauraAdisty <162139041+HauraAdisty@users.noreply.github.com>
2025-05-09 23:45:00 +0700

    layouts

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
+
 
 class MovieController extends Controller
 {
-    //
+    public function index() 
+    {
+        $movies = Movie::select('title', 'synopsis', 'cover_image')->paginate(6);
+        return view('movie.index', compact('movies'));
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Query pencarian berdasarkan title
+        $movies = Movie::where('title', 'LIKE', "%{$query}%")->paginate(6);
+
+        return view('movie.index', compact('movies'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/search', [MovieController::class, 'search'])->name('movies.search');
